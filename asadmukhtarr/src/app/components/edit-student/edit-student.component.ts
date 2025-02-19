cb4f5f61ced13982a77b5ace055705910ce701f8
asadmukhtarr <axad03213@gmail.com>
2025-02-19 19:49:55 +0500

    edit functionaly with update

--- asadmukhtarr/src/app/components/edit-student/edit-student.component.ts
+++ asadmukhtarr/src/app/components/edit-student/edit-student.component.ts
@@ -1,16 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // ✅ Import FormsModule
 
 @Component({
   selector: 'app-edit-student',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './edit-student.component.html',
   styleUrl: './edit-student.component.css'
 })
 export class EditStudentComponent {
   studentId: any = null;
-  student: any = null;
+  student: any = {
+    name: '',
+    email: '',
+    city: '',
+    whatsapp: ''
+  };
   constructor(private route:ActivatedRoute){
   }
   ngOnInit(){
@@ -20,8 +27,19 @@ export class EditStudentComponent {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-      this.student = data,
-      console.log(this.student)
+      this.student = data
     });
+  }
+  updateData = () => {
+    let url = `https://67acb30c3f5a4e1477db8d92.mockapi.io/students/students/${this.studentId}`
+    fetch(url,{
+      method:"PUT",
+      headers:{
+        "content-type" : "application/json"
+      },
+      body:JSON.stringify(this.student)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
   }
 }
